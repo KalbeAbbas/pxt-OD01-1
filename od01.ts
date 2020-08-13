de4ebe7affe58df54d632501c176f3e8fb86ee2c
KalbeAbbas <kalbeabbas142@gmail.com>
2020-08-13 17:51:14 +0500

    Update od01.ts

--- od01.ts
+++ od01.ts
@@ -112,8 +112,8 @@ namespace OD01 {
         for (let i = 0; i < 5; i++) {
             _screen[ind + i] = (color > 0) ? Font_5x7[p + i] : Font_5x7[p + i] ^ 0xFF
 
-            higher_byte = (_screen[ind + i] & 0x80) | ((_screen[ind + i] & 0x80) >> 1) | (_screen[ind + i] & 0x40) | ((_screen[ind + i] & 0x40) >> 1) | (_screen[ind + i] & 0x20) | ((_screen[ind + i] & 0x20) >> 1) | (_screen[ind + i] & 0x10) | ((_screen[ind + i] & 0x10) >> 1)
-            lower_byte = (_screen[ind + i] & 0x08) | ((_screen[ind + i] & 0x08) >> 1) | (_screen[ind + i] & 0x04) | ((_screen[ind + i] & 0x04) >> 1) | (_screen[ind + i] & 0x02) | ((_screen[ind + i] & 0x02) >> 1) | (_screen[ind + i] & 0x01) | ((_screen[ind + i] & 0x01) >> 1)
+            lower_byte = (_screen[ind + i] & 0x80) | ((_screen[ind + i] & 0x80) >> 1) | (_screen[ind + i] & 0x40) | ((_screen[ind + i] & 0x40) >> 1) | (_screen[ind + i] & 0x20) | ((_screen[ind + i] & 0x20) >> 1) | (_screen[ind + i] & 0x10) | ((_screen[ind + i] & 0x10) >> 1)
+            higher_byte = (_screen[ind + i] & 0x08) | ((_screen[ind + i] & 0x08) >> 1) | (_screen[ind + i] & 0x04) | ((_screen[ind + i] & 0x04) >> 1) | (_screen[ind + i] & 0x02) | ((_screen[ind + i] & 0x02) >> 1) | (_screen[ind + i] & 0x01) | ((_screen[ind + i] & 0x01) >> 1)
 
             _buf7[j + 1] = higher_byte
             _buf7[j + 2] = higher_byte
