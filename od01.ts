665f51338ac18aadf81fda6817ba2bb6861d3ac4
KalbeAbbas <kalbeabbas142@gmail.com>
2020-08-13 10:55:01 +0500

    Update od01.ts

--- od01.ts
+++ od01.ts
@@ -6,137 +6,7 @@
 
 // 6x8 font
 // Each 7 bit or literal in the font is 1/5th of the font
-//const Font_5x7 = hex`000000000000005F00000007000700147F147F14242A072A12231308646237495522500005030000001C2241000041221C00082A1C2A0808083E080800503000000808080808006060000020100804023E5149453E00427F400042615149462141454B311814127F1027454545393C4A49493001710905033649494936064949291E003636000000563600000008142241141414141441221408000201510906324979413E7E1111117E7F494949363E414141227F4141221C7F494949417F090901013E414151327F0808087F00417F41002040413F017F081422417F404040407F0204027F7F0408107F3E4141413E7F090909063E4151215E7F09192946464949493101017F01013F4040403F1F2040201F7F2018207F63140814630304780403615149454300007F4141020408102041417F000004020102044040404040000102040020545454787F484444383844444420384444487F3854545418087E090102081454543C7F0804047800447D40002040443D00007F10284400417F40007C041804787C0804047838444444387C14141408081414187C7C080404084854545420043F4440203C4040207C1C2040201C3C4030403C44281028440C5050503C4464544C44000836410000007F000000413608000201020402`
-
-   let font: number[] = [];
-    font[0] = 0x0022d422;
-    font[1] = 0x0022d422;
-    font[2] = 0x0022d422;
-    font[3] = 0x0022d422;
-    font[4] = 0x0022d422;
-    font[5] = 0x0022d422;
-    font[6] = 0x0022d422;
-    font[7] = 0x0022d422;
-    font[8] = 0x0022d422;
-    font[9] = 0x0022d422;
-    font[10] = 0x0022d422;
-    font[11] = 0x0022d422;
-    font[12] = 0x0022d422;
-    font[13] = 0x0022d422;
-    font[14] = 0x0022d422;
-    font[15] = 0x0022d422;
-    font[16] = 0x0022d422;
-    font[17] = 0x0022d422;
-    font[18] = 0x0022d422;
-    font[19] = 0x0022d422;
-    font[20] = 0x0022d422;
-    font[21] = 0x0022d422;
-    font[22] = 0x0022d422;
-    font[23] = 0x0022d422;
-    font[24] = 0x0022d422;
-    font[25] = 0x0022d422;
-    font[26] = 0x0022d422;
-    font[27] = 0x0022d422;
-    font[28] = 0x0022d422;
-    font[29] = 0x0022d422;
-    font[30] = 0x0022d422;
-    font[31] = 0x0022d422;
-    font[32] = 0x00000000;
-    font[33] = 0x000002e0;
-    font[34] = 0x00018060;
-    font[35] = 0x00afabea;
-    font[36] = 0x00aed6ea;
-    font[37] = 0x01991133;
-    font[38] = 0x010556aa;
-    font[39] = 0x00000060;
-    font[40] = 0x000045c0;
-    font[41] = 0x00003a20;
-    font[42] = 0x00051140;
-    font[43] = 0x00023880;
-    font[44] = 0x00002200;
-    font[45] = 0x00021080;
-    font[46] = 0x00000100;
-    font[47] = 0x00111110;
-    font[48] = 0x0007462e;
-    font[49] = 0x00087e40;
-    font[50] = 0x000956b9;
-    font[51] = 0x0005d629;
-    font[52] = 0x008fa54c;
-    font[53] = 0x009ad6b7;
-    font[54] = 0x008ada88;
-    font[55] = 0x00119531;
-    font[56] = 0x00aad6aa;
-    font[57] = 0x0022b6a2;
-    font[58] = 0x00000140;
-    font[59] = 0x00002a00;
-    font[60] = 0x0008a880;
-    font[61] = 0x00052940;
-    font[62] = 0x00022a20;
-    font[63] = 0x0022d422;
-    font[64] = 0x00e4d62e;
-    font[65] = 0x000f14be;
-    font[66] = 0x000556bf;
-    font[67] = 0x0008c62e;
-    font[68] = 0x0007463f;
-    font[69] = 0x0008d6bf;
-    font[70] = 0x000094bf;
-    font[71] = 0x00cac62e;
-    font[72] = 0x000f909f;
-    font[73] = 0x000047f1;
-    font[74] = 0x0017c629;
-    font[75] = 0x0008a89f;
-    font[76] = 0x0008421f;
-    font[77] = 0x01f1105f;
-    font[78] = 0x01f4105f;
-    font[79] = 0x0007462e;
-    font[80] = 0x000114bf;
-    font[81] = 0x000b6526;
-    font[82] = 0x010514bf;
-    font[83] = 0x0004d6b2;
-    font[84] = 0x0010fc21;
-    font[85] = 0x0007c20f;
-    font[86] = 0x00744107;
-    font[87] = 0x01f4111f;
-    font[88] = 0x000d909b;
-    font[89] = 0x00117041;
-    font[90] = 0x0008ceb9;
-    font[91] = 0x0008c7e0;
-    font[92] = 0x01041041;
-    font[93] = 0x000fc620;
-    font[94] = 0x00010440;
-    font[95] = 0x01084210;
-    font[96] = 0x00000820;
-    font[97] = 0x010f4a4c;
-    font[98] = 0x0004529f;
-    font[99] = 0x00094a4c;
-    font[100] = 0x000fd288;
-    font[101] = 0x000956ae;
-    font[102] = 0x000097c4;
-    font[103] = 0x0007d6a2;
-    font[104] = 0x000c109f;
-    font[105] = 0x000003a0;
-    font[106] = 0x0006c200;
-    font[107] = 0x0008289f;
-    font[108] = 0x000841e0;
-    font[109] = 0x01e1105e;
-    font[110] = 0x000e085e;
-    font[111] = 0x00064a4c;
-    font[112] = 0x0002295e;
-    font[113] = 0x000f2944;
-    font[114] = 0x0001085c;
-    font[115] = 0x00012a90;
-    font[116] = 0x010a51e0;
-    font[117] = 0x010f420e;
-    font[118] = 0x00644106;
-    font[119] = 0x01e8221e;
-    font[120] = 0x00093192;
-    font[121] = 0x00222292;
-    font[122] = 0x00095b52;
-    font[123] = 0x0008fc80;
-    font[124] = 0x000003e0;
-    font[125] = 0x000013f1;
-    font[126] = 0x00841080;
-    font[127] = 0x0022d422;
+const Font_5x7 = hex`000000000000005F00000007000700147F147F14242A072A12231308646237495522500005030000001C2241000041221C00082A1C2A0808083E080800503000000808080808006060000020100804023E5149453E00427F400042615149462141454B311814127F1027454545393C4A49493001710905033649494936064949291E003636000000563600000008142241141414141441221408000201510906324979413E7E1111117E7F494949363E414141227F4141221C7F494949417F090901013E414151327F0808087F00417F41002040413F017F081422417F404040407F0204027F7F0408107F3E4141413E7F090909063E4151215E7F09192946464949493101017F01013F4040403F1F2040201F7F2018207F63140814630304780403615149454300007F4141020408102041417F000004020102044040404040000102040020545454787F484444383844444420384444487F3854545418087E090102081454543C7F0804047800447D40002040443D00007F10284400417F40007C041804787C0804047838444444387C14141408081414187C7C080404084854545420043F4440203C4040207C1C2040201C3C4030403C44281028440C5050503C4464544C44000836410000007F000000413608000201020402`
 
 //% color=#444444 icon="\uf26c" block="OD01"
 //% groups='["Scrolling Display", "Positional Display", "Drawing", "Optional"]'
@@ -158,7 +28,7 @@ namespace OD01 {
     let _buf2 = pins.createBuffer(2)
     let _buf3 = pins.createBuffer(3)
     let _buf4 = pins.createBuffer(4)
-    let _buf7 = pins.createBuffer(13)
+    let _buf7 = pins.createBuffer(7)
     _buf7[0] = 0x40
     export let _DRAW = 1
     let _cx = 0
@@ -232,33 +102,19 @@ namespace OD01 {
         }
     }
 
-    /*function char(c: string, col: number, row: number, color: number = 1) {
-        let col = 0
-        let p = 0
-        let ind = 0
+    function char(c: string, col: number, row: number, color: number = 1) {
+        let p = (Math.min(127, Math.max(c.charCodeAt(0), 32)) - 32) * 5
+        let ind = col + row * 128 + 1
 
-        for (let n = 0; n < c.length; n++) {
-            p = font[c.charCodeAt(n)]
-            for (let i = 0; i < 5; i++) {
-                col = 0
-                for (let j = 0; j < 5; j++) {
-                    if (p & (1 << (5 * i + j)))
-                        col |= (1 << (j + 1))
-                }
-                ind = (x + n) * 5 * (_ZOOM + 1) + y * 128 + i * (_ZOOM + 1) + 1
-                if (color == 0)
-                    col = 255 - col
-                _screen[ind] = col
-                if (_ZOOM)
-                    _screen[ind + 1] = col
-            }
+        for (let i = 0; i < 5; i++) {
+            _screen[ind + i] = (color > 0) ? Font_5x7[p + i] : Font_5x7[p + i] ^ 0xFF
+            _buf7[i + 1] = _screen[ind + i]
         }
-        set_pos(x * 5, y)
-        let ind0 = x * 5 * (_ZOOM + 1) + y * 128
-        let buf = _screen.slice(ind0, ind + 1)
-        buf[0] = 0x40
-        pins.i2cWriteBuffer(_I2CAddr, buf)
-    }*/
+        _screen[ind + 5] = (color > 0) ? 0 : 0xFF
+        _buf7[6] = _screen[ind + 5]
+        set_pos(col, row)
+        pins.i2cWriteBuffer(_I2CAddr, _buf7)
+    }
 
     /**
      * show text at a specific location on screen. Note - to remove the text print an empty string ("  ") to the same location.
@@ -270,31 +126,11 @@ namespace OD01 {
     //% color.max=1 color.min=0 color.defl=1
     //% weight=50 blockGap=8 inlineInputMode=inline
     //% group="Positional Display"
-    export function showString(s: string, x: number, y: number, color: number = 1) {
-        let col = 0
-        let p = 0
-        let ind = 0
+    export function showString(s: string, col: number, row: number, color: number = 1) {
         for (let n = 0; n < s.length; n++) {
-            p = font[s.charCodeAt(n)]
-            for (let i = 0; i < 5; i++) {
-                col = 0
-                for (let j = 0; j < 5; j++) {
-                    if (p & (1 << (5 * i + j)))
-                        col |= (1 << (j + 1))
-                }
-                ind = (x + n) * 5 * (_ZOOM + 1) + y * 128 + i * (_ZOOM + 1) + 1
-                if (color == 0)
-                    col = 255 - col
-                _screen[ind] = col
-                if (_ZOOM)
-                    _screen[ind + 1] = col
-            }
+            char(s.charAt(n), col, row, color)
+            col += 6
         }
-        set_pos(x * 5, y)
-        let ind0 = x * 5 * (_ZOOM + 1) + y * 128
-        let buf = _screen.slice(ind0, ind + 1)
-        buf[0] = 0x40
-        pins.i2cWriteBuffer(_I2CAddr, buf)
     }
 
     /**
