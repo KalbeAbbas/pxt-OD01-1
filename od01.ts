53fe23a35318eaac3c938080d9332dd28c2d6170
KalbeAbbas <kalbeabbas142@gmail.com>
2020-08-17 11:20:21 +0500

    Update od01.ts

--- od01.ts
+++ od01.ts
@@ -140,14 +140,13 @@ namespace OD01 {
     export function showString(s: string, col: number, row: number, color: number = 1) {
         for (let n = 0; n < s.length; n++) {
             char(s.charAt(n), col, row, color)
-            /*if(_ZOOM)
+            if(_ZOOM)
             {
                 col += 12
             }else{
                 col += 6
-            }*/
+            }
 
-            col += 6
         }
     }
 
