e3f4ad77019cecb7337153f20fe090bea5204bfd
KalbeAbbas <kalbeabbas142@gmail.com>
2020-08-17 11:19:07 +0500

    Update od01.ts

--- od01.ts
+++ od01.ts
@@ -106,13 +106,23 @@ namespace OD01 {
     function char(c: string, col: number, row: number, color: number = 1) {
         let p = (Math.min(127, Math.max(c.charCodeAt(0), 32)) - 32) * 5
         let ind = col + row * 128 + 1
+        let j = 0
 
         for (let i = 0; i < 5; i++) {
             _screen[ind + i] = (color > 0) ? Font_5x7[p + i] : Font_5x7[p + i] ^ 0xFF
+
             _buf7[i + 1] = _screen[ind + i]
+            
+            if(_ZOOM)
+                _buf7[i + 2] = _screen[ind + i]
+
+            j += 2
         }
+
         _screen[ind + 5] = (color > 0) ? 0 : 0xFF
+
         _buf7[6] = _screen[ind + 5]
+        
         set_pos(col, row)
         pins.i2cWriteBuffer(_I2CAddr, _buf7)
     }
