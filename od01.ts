03e6aae54e6df76c767f80b650cb44fe075a7673
KalbeAbbas <kalbeabbas142@gmail.com>
2020-11-02 18:50:56 +0500

    Update od01.ts

--- od01.ts
+++ od01.ts
@@ -123,16 +123,12 @@ namespace OD01 {
     //% weight=30 blockGap=8
     //% group="Drawing"
     export function pixel(x: number, y: number, color: number = 1) {
-        /*let page = y >> 3
+        let page = y >> 3
         let shift_page = y % 8
-        let ind = x + page * 128 + 1*/
+        let ind = x + page * 128 + 1
 
-
-        //let page = y >> 3
-        let shift_page = y % 8
-        let ind = x + (y >>3) * 128 + 1
-        //let b = (color) ? (_screen[ind] | (1 << shift_page)) : clrbit(_screen[ind], shift_page)
-        _screen[ind] = (color) ? (_screen[ind] | (1 << shift_page)) : clrbit(_screen[ind], shift_page)
+        let b = (color) ? (_screen[ind] | (1 << shift_page)) : clrbit(_screen[ind], shift_page)
+        _screen[ind] = b
         /*if (_DRAW) {
             set_pos(x, page)
             _buf2[0] = 0x40
